6db575f9829bb40ddcbcc5de36cee60cd9582ca9
sklbz <122096315+sklbz@users.noreply.github.com>
2025-01-15 14:48:16 +0100

    Update macro_rules.rs

--- src/macro_rules.rs
+++ src/macro_rules.rs
@@ -6,3 +6,20 @@ macro_rules! say_hello {
         println!("Hello!")
     };
 }
+
+macro_rules! create_function {
+    ($func_name:ident) => {
+        fn $func_name() {
+            println!("You called {:?}()",
+                     stringify!($func_name));
+        }
+    };
+}
+
+macro_rules! print_result {
+    ($expression:expr) => {
+        println!("{:?} = {:?}",
+                 stringify!($expression),
+                 $expression);
+    };
+}
